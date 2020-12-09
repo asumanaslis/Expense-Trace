cf5c0ae63564dc0092ee79ce85ede9b7082ebc02
asumanaslis <61350777+asumanaslis@users.noreply.github.com>
2020-12-09 11:41:12 +0300

    homescreen added

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Home = (props) => {
+    return (
+        <View>
+            <Text>HomeScreen</Text>
+        </View>
+    );
+};
+export default Home;
