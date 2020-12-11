e8d714b60616c8db374694ebc6cbd2c1d6dd2d6b
asumanaslis <61350777+asumanaslis@users.noreply.github.com>
2020-12-12 00:53:02 +0300

    reducer update

--- src/redux/Auth/reducer.js
+++ src/redux/Auth/reducer.js
@@ -1,27 +1,36 @@
-import {LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILED} from './types';
+import { LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILED, REGISTER_START } from './types';
 
 const INITIAL_STATE = {
   isAuth: false,
-  loading: false,
+  loading: false
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case LOGIN_START:
+    case REGISTER_START:
       return {
         ...state,
         isAuth: action.payload,
         loading: true,
       };
+    case LOGIN_START:
+      return {
+        ...state,
+        isAuth: action.payload,
+        loading: true,
+        isAuth: false
+      };
     case LOGIN_SUCCESS:
       return {
         ...state,
         loading: false,
+        isAuth: true
       };
     case LOGIN_FAILED:
       return {
         ...state,
         loading: false,
+        isAuth: false
       };
     default:
       return state;
