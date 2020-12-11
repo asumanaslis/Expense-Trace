9f5c84b6a19d8f1a369685f370d16e5a6f2397a2
asumanaslis <61350777+asumanaslis@users.noreply.github.com>
2020-12-12 00:52:43 +0300

    actions added

---
+++ src/redux/Auth/actions.js
@@ -0,0 +1,41 @@
+import auth from '@react-native-firebase/auth';
+import { REGISTER_START, REGISTER_SUCCESS, LOGIN_START, LOGIN_SUCCESS, LOGIN_FAILED } from './types';
+
+
+export const register = (email, password) => {
+    return async (dispatch) => {
+        dispatch({ type: REGISTER_START });
+        await auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(() => {
+                console.log('User account created & signed in!');
+                dispatch({ type: REGISTER_SUCCESS });
+            })
+            .catch(error => {
+                if (error.code === 'auth/email-already-in-use') {
+                    console.log('That email is already in use');
+                }
+                if (error.code === 'auth/invalid-email') {
+                    console.log('That email address is invalid!');
+                }
+                console.error(error);
+            })
+    };
+};
+
+export const login = (email, password) => {
+    return async (dispatch) => {
+        dispatch({ type: LOGIN_START });
+        await auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(async () => {
+                dispatch({ type: LOGIN_SUCCESS });
+            })
+            .catch((error) => {
+                dispatch({ type: LOGIN_FAILED });
+                console.log('login failed')
+            })
+
+    };
+};
+
