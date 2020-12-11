35f38e73faca5af45e18607dfec98731703738a7
asumanaslis <61350777+asumanaslis@users.noreply.github.com>
2020-12-12 00:51:55 +0300

    signup screen update

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import {View, Text, SafeAreaView, StyleSheet, ImageBackground} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, SafeAreaView, StyleSheet, ImageBackground } from 'react-native';
 import Input from '../components/Input';
 import Button from '../components/Button';
-import {RFValue} from 'react-native-responsive-fontsize';
-import {useNavigation} from '@react-navigation/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { useNavigation } from '@react-navigation/native';
+import { register } from '../redux/Auth/actions';
+import { useDispatch } from 'react-redux';
 
 const SignUpScreen = () => {
+  const dispatch = useDispatch();
+  const registerUser = (email, password) => {
+    dispatch(register(email, password));
+  }
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const navigation = useNavigation();
   return (
     <SafeAreaView style={styles.safeAreaView}>
@@ -16,9 +25,9 @@ const SignUpScreen = () => {
         <View style={styles.formView}>
           <View style={styles.formContentContainer}>
             <Text style={styles.Register}>Register</Text>
-            <Input label="Full Name" style={{marginBottom: '4%'}}/>
-            <Input label="Email" style={{marginBottom: '4%'}} />
-            <Input label="Password" secureTextEntry={true} />
+            <Input label="Full Name" style={{ marginBottom: '4%' }} onChangeText={(name) => setName(name)} />
+            <Input label="Email" style={{ marginBottom: '4%' }} onChangeText={(i) => setEmail(i)} />
+            <Input label="Password" secureTextEntry={true} onChangeText={(pass) => setPassword(pass)} />
           </View>
         </View>
         {/*Footer Section*/}
@@ -33,7 +42,10 @@ const SignUpScreen = () => {
                 Login
               </Text>
             </Text>
-            <Button buttonName="Register" onPress={() => navigation.navigate("SignIn")}/>
+            <Button buttonName="Register" onPress={() => {
+              registerUser(email, password);
+              navigation.navigate('Home')
+            }} />
           </View>
         </View>
       </ImageBackground>
@@ -41,7 +53,7 @@ const SignUpScreen = () => {
   );
 };
 const styles = StyleSheet.create({
-  boldText: {fontWeight: 'bold'},
+  boldText: { fontWeight: 'bold' },
   registerText: {
     fontSize: RFValue(15),
     color: '#fff',
@@ -53,7 +65,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     fontWeight: 'bold',
   },
-  formContentContainer: {width: '82%', marginLeft: '7%'},
+  formContentContainer: { width: '82%', marginLeft: '7%' },
   footerComponentContainer: {
     flexDirection: 'row',
     alignItems: 'flex-end',
@@ -62,13 +74,13 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     marginBottom: '2%',
   },
-  footerView: {flex: 2, justifyContent: 'flex-end'},
+  footerView: { flex: 2, justifyContent: 'flex-end' },
   formView: {
     flex: 7,
     justifyContent: 'flex-end',
   },
-  mainView: {flex: 1, backgroundColor: '#E5E5E5'},
-  safeAreaView: {flex: 1, backgroundColor: '#2F52E0'},
+  mainView: { flex: 1, backgroundColor: '#E5E5E5' },
+  safeAreaView: { flex: 1, backgroundColor: '#2F52E0' },
 });
 
 export default SignUpScreen;
